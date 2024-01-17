0242fe3f0379f983eddee0171ecec89dfbd3c521
Mahmoud-Abdulsalam <142223217+Mahmoud-Abdulsalam@users.noreply.github.com>
2024-01-17 21:41:59 +0100

    add Check for division by zero

--- index.js
+++ index.js
@@ -3,14 +3,30 @@
 const display = document.getElementById("display");
 function appendtodisplay(input) {
   display.value += input;
+  errorMessage.classList.add("hidden");
 }
 function clearDisplay() {
   display.value = "";
+  errorMessage.classList.add("hidden");
 }
 function calculate() {
   try {
     display.value = eval(display.value);
   } catch (error) {
     display.value = "Erro";
+  }
+}
+function calculate() {
+  try {
+    const expression = display.value;
+
+    // Check for division by zero 
+    if (expression.includes("/0")) {
+      display.value = "can't Div by 0";
+    } else {
+      display.value = eval(expression);
+    }
+  } catch (error) {
+    display.value = "Error";
   }
 }
